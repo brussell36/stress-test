5146b830101d7e2956a6f070a5248beb98cabc8b
Ben Russell <benjaminrussell36@gmail.com>
2020-06-09 15:19:17 -0700

    Change if else logic to try to properly show all resources.
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
     event.preventDefault();
     $("#responses").show();
     $("input:checkbox[name=stress-symptoms]:checked").each(function() {
-      const stressSymptom = $(this).val();
+      let stressSymptom = $(this).val();
       $("#responses").append(stressSymptom + "<br>");
-      if (stressSymptom === 'Shallow breathing' || 'Impending sense of doom' || 'Inabiility to form coherent sentences' || 'Finding yourself in an unfamiliar place without any idea how you got there' || 'Itchy eyeballs' || "Walking the dog at 3am, but you don't have a dog" || 'Sweating profusely' || 'Buying things compulsively on the internet') {
+      if (stressSymptom === 'Shallow breathing') {
         $(".Resource1").show();
-      }
-      else if (stressSymptom === 'Shallow breathing' && 'Impending sense of doom' || 'Inabiility to form coherent sentences' && 'Finding yourself in an unfamiliar place without any idea how you got there' || 'Itchy eyeballs' && "Walking the dog at 3am, but you don't have a dog" || 'Sweating profusely' && 'Buying things compulsively on the internet') {
+      } else if (stressSymptom === 'Impending sense of doom') {
         $(".Resource2").show();
       } else {
         $(".Resource3").show();
