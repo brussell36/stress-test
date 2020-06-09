e1e416297a8e39064693ec1bdeb951f5630963b3
Ben Russell <benjaminrussell36@gmail.com>
2020-06-09 14:42:56 -0700

    Add If Else statement to show resources for different levels of stress.
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,18 @@ $(document).ready(function() {
     $("input:checkbox[name=stress-symptoms]:checked").each(function() {
       const stressSymptom = $(this).val();
       $("#responses").append(stressSymptom + "<br>");
+      if (stressSymptom === 'Shallow breathing' || 'Impending sense of doom' || 'Inabiility to form coherent sentences' || 'Finding yourself in an unfamiliar place without any idea how you got there' || 'Itchy eyeballs' || "Walking the dog at 3am, but you don't have a dog" || 'Sweating profusely' || 'Buying things compulsively on the internet') {
+        $(".Resource1").show();
+      }
+      else if (stressSymptom === 'Shallow breathing' && 'Impending sense of doom' || 'Inabiility to form coherent sentences' && 'Finding yourself in an unfamiliar place without any idea how you got there' || 'Itchy eyeballs' && "Walking the dog at 3am, but you don't have a dog" || 'Sweating profusely' && 'Buying things compulsively on the internet') {
+        $(".Resource2").show();
+      } else {
+        $(".Resource3").show();
+      }
     });
-    $('#stress-test').hide();
+    $('#stress-test').hide();   
   });
 });
+
+
+
